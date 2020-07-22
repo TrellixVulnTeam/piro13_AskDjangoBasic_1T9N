0f8e22532cf27653b72f0846192cc94fa5c2ef05
InjiChoi <chldlswl1212@gmail.com>
2020-07-23 00:21:02 +0900

    10 장고 모델 필드

--- shop/models.py
+++ shop/models.py
@@ -9,3 +9,14 @@ class Item(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+# #blog model
+# class Post(models.Model):
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=100, db_index=True)
+#     slug = models.SlugField(allow_unicode=True, db_index=True)
+#     content = models.TextField(blank=True)
+#     image = models.ImageField(blank=True)
+#     comment_count = models.PositiveIntegerField(default=0)
+#     is_publish = models.BooleanField(default=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
