a08576d5743da9be7d5be3842d72d2a42ee035e9
InjiChoi <chldlswl1212@gmail.com>
2020-07-23 01:29:30 +0900

    12 장고 Admin을 통한 데이터 관리

--- shop/models.py
+++ shop/models.py
@@ -5,8 +5,12 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True) #길이제한 없음
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 # #blog model
