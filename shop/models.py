27013053e7b08accbc567f40241cf441f0219234
InjiChoi <chldlswl1212@gmail.com>
2020-07-22 23:38:32 +0900

    09 장고 모델 (ORM)

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True) #길이제한 없음
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
