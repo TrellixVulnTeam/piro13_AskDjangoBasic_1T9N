6900bd9f9153d654afac7afae77b670aecebb6f1
InjiChoi <chldlswl1212@gmail.com>
2020-07-24 22:33:37 +0900

    28 URL Reverse를 통해 유연하게 URL 생성하기

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from shop.models import Item
 import logging
