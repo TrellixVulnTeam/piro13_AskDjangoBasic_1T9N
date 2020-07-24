0050e0f6c3b70fb6c943f918318a27f83bf940c0
InjiChoi <chldlswl1212@gmail.com>
2020-07-24 22:59:05 +0900

    29 장고 Form을 쓰지 않고, 글 생성/수정 구현하기

--- shop/urls.py
+++ shop/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('item/', views.item_list, name='item_list'),
     path('item/<int:pk>/', views.item_detail, name='item_detail'),
     # re_path(r'^(?P<pk>\d+)/$', views.item_detail),
+    path('item/new/', views.item_new, name='item_new'),
+    path('item/<int:pk>/edit', views.item_edit, name='item_edit'),
 ]
